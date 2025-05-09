722d4b9d1c1f2903d36e050d56e4dc4474788657
Danny Regan <d.regan@columbia.edu>
2025-05-09 21:50:59 +0200

    Comments added to util func.

--- db_utils.py
+++ db_utils.py
@@ -1,3 +1,6 @@
+# A helper function to ensure connection is maintained with the database
+# I wasn't sure why, but the database routinely lost connection when buttons were clicked
+# This function prevents that from crashing the app
 def ensure_connection(connection):
     if not connection.is_connected():
         connection.reconnect()
